e1ef25736f22bb0090b8edc45e6349da72dc49ae
rayegg111 <55218513+rayegg111@users.noreply.github.com>
2019-11-07 10:37:44 +0800

    3

---
+++ UnityProject/Assets/robotNPC.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class robotNPC : MonoBehaviour
+{
+    [Header("任務對話")]
+    public string star = "嘿小家伙，你能幫我找個電池嗎?";
+    public string complete = "謝了小兄弟";
+    [Header("對話速度")]
+    public float talkspeed = 1f;
+    [Header("任務")]
+    public bool mission_complete = false;
+    public int count_player = 0;
+    public int finish = 5;
+}
---
+++ UnityProject/Assets/robotball.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+
+public class robotball : MonoBehaviour
+{
+    [Header("移動速度")]
+    public float speed = 5f;
+    [Header("跳躍高度")]
+    public float jump = 2f;
+
+    public bool pass = false;
+    public bool isGround = false;
+
+    private Rigidbody rig;
+    private Transform tra;
+
+
+    private void Start()
+    {
+        rig = GetComponent<Rigidbody>();
+        tra = GetComponent<Transform>();
+    }
+
+    private void FixedUpdate()
+    {
+        Walk();
+        //Jump();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        isGround = true;
+        Debug.Log("碰到" + collision.gameObject);
+    }
+
+    /// <summary>
+    /// 走路
+    /// </summary>
+    void Walk()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal * speed, 0, moveVertical * speed);
+    }
+    /// <summary>
+    /// 跳躍
+    /// </summary>
+    void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isGround == true)
+        {
+            isGround = false;
+            rig.AddForce(new Vector3(0, 0, jump));
+        }
+    }
+}
